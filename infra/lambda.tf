548820739b430c37ffa7d7334e0f361123c8ed0a
Josh M <lollipop_josh@hotmail.co.uk>
2026-09-11 23:53:46 +0100

    adding cloudwatch logs to lambda function for terraform. Uses local for fixed/constant lambda name to reference to

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,26 +1,36 @@
+locals {
+  weather_ingest_function_name = "weather_ingest_lambda"
+}
+
+resource "aws_cloudwatch_log_group" "weather_ingest" {
+  name              = "/aws/lambda/${local.weather_ingest_function_name}"
+  retention_in_days = 14
+}
+
 data "archive_file" "layer_zip" {
-    type        = "zip"
-    source_dir  = "${path.module}/../build/layer"
-    output_path = "${path.module}/../build/layer.zip"
-    excludes    = [
-        "**/__pycache__/**",
-        "**/tests/**",
-        "python/bin/**",
-    ]
+  type        = "zip"
+  source_dir  = "${path.module}/../build/layer"
+  output_path = "${path.module}/../build/layer.zip"
+  excludes    = [
+      "**/__pycache__/**",
+      "**/tests/**",
+      "python/bin/**",
+  ]
 }
 
 data "archive_file" "code_zip" {
-    type        = "zip"
-    source_dir  = "${path.module}/../src"
-    output_path = "${path.module}/../build/code.zip"
-    excludes    = ["ml/**", "**/__pycache__/**"]
+  type        = "zip"
+  source_dir  = "${path.module}/../src"
+  output_path = "${path.module}/../build/code.zip"
+  excludes    = ["ml/**", "**/__pycache__/**"]
 }
 
 resource "aws_lambda_layer_version" "ingest_lambda_layer" {
-    filename   = data.archive_file.layer_zip.output_path
-    layer_name = "ingest_lambda_layer"
+  filename   = data.archive_file.layer_zip.output_path
+  layer_name = "ingest_lambda_layer"
 
-    source_code_hash         = data.archive_file.layer_zip.output_base64sha256
-    compatible_runtimes      = ["python3.12"]
-    compatible_architectures = ["x86_64"]
+  source_code_hash         = data.archive_file.layer_zip.output_base64sha256
+  compatible_runtimes      = ["python3.12"]
+  compatible_architectures = ["x86_64"]
 }
+
