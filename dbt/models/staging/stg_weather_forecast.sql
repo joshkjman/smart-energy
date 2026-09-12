2e25c77b4caf138b5aa1910400ab5c890eeff330
Josh M <lollipop_josh@hotmail.co.uk>
2026-09-13 00:48:25 +0100

    removed n from ingestion, so staging needs to fill in lead days with deriving it from the target and issue timestamps

--- dbt/models/staging/stg_weather_forecast.sql
+++ dbt/models/staging/stg_weather_forecast.sql
@@ -1,13 +1,16 @@
 with raw as (
-    select r
+    select  cast(from_iso8601_timestamp(r.target_ts) at time zone 'UTC' as timestamp) as target_ts,
+            cast(from_iso8601_timestamp(r.issue_ts) at time zone 'UTC' as timestamp) as issue_ts,
+            r.variable,
+            r.value
     from {{ source('bronze', 'weather_forecast') }}
     cross join unnest(data) as t(r)
 )
 select
-    cast(from_iso8601_timestamp(r.target_ts) at time zone 'UTC' as timestamp) as target_ts,
-    cast(from_iso8601_timestamp(r.issue_ts) at time zone 'UTC' as timestamp) as issue_ts,
-    r.n as lead_days,
-    max(r.value) filter (where regexp_replace(r.variable, '_previous_day\d+$', '') = 'temperature_2m') as temperature_2m,
-    max(r.value) filter (where regexp_replace(r.variable, '_previous_day\d+$', '') = 'shortwave_radiation') as shortwave_radiation
+    target_ts,
+    issue_ts,
+    date_diff('day', issue_ts, date_trunc('day', target_ts)) as lead_days,
+    max(value) filter (where regexp_replace(variable, '_previous_day\d+$', '') = 'temperature_2m') as temperature_2m,
+    max(value) filter (where regexp_replace(variable, '_previous_day\d+$', '') = 'shortwave_radiation') as shortwave_radiation
 from raw
 group by 1, 2, 3
