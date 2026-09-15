d1b399c5687151fcc2334044930c49ae5cc0198e
Josh M <lollipop_josh@hotmail.co.uk>
2026-09-15 16:36:47 +0100

    Adding terraform for creating eventbridge rules to trigger ingestion lambdas for both weather and demand. Weather every morning at 5am so the 00:00 issue stamp stays near-honest at lead 0 and you dont end up with leakage and then demand at 07:00am since its trailing window makes timing irrelevant.

---
+++ infra/events.tf
@@ -0,0 +1,41 @@
+resource "aws_cloudwatch_event_rule" "weather_ingest" {
+  name                = "weather_ingest_event"
+  description         = "Invoke lambda for weather ingestion from API"
+  schedule_expression = "cron(0 5 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "weather_ingest" {
+  rule      = aws_cloudwatch_event_rule.weather_ingest.name
+  target_id = "weather_ingest_event_target"
+  arn       = aws_lambda_function.weather_ingest_lambda_function.arn
+}
+
+resource "aws_lambda_permission" "weather_ingest" {
+  statement_id  = "AllowExecutionFromEvents"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.weather_ingest_lambda_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.weather_ingest.arn
+}
+
+
+
+resource "aws_cloudwatch_event_rule" "demand_ingest" {
+  name                = "demand_ingest_event"
+  description         = "Invoke lambda for demand ingestion from API"
+  schedule_expression = "cron(0 7 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "demand_ingest" {
+  rule      = aws_cloudwatch_event_rule.demand_ingest.name
+  target_id = "demand_ingest_event_target"
+  arn       = aws_lambda_function.demand_ingest_lambda_function.arn
+}
+
+resource "aws_lambda_permission" "demand_ingest" {
+  statement_id  = "AllowExecutionFromEvents"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.demand_ingest_lambda_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.demand_ingest.arn
+}
